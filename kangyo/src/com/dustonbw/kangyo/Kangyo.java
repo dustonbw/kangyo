d7a7f6da06d98d2497cdfaa5bea2214ebdac59a2
Duston Baranow-Watts <100014076+dustonbw@users.noreply.github.com>
2025-03-26 17:19:11 -0400

    fix typo

--- kangyo/src/com/dustonbw/kangyo/Kangyo.java
+++ kangyo/src/com/dustonbw/kangyo/Kangyo.java
@@ -15,7 +15,7 @@ public class Kangyo {
 
 	public static void main(String[] args) throws IOException {
 		if (args.length > 1) {
-			System.out.println("Usage: tanjyun [script]");
+			System.out.println("Usage: kangyo [script]");
 			System.exit(64);
 		} else if (args.length == 1) {
 			runFile(args[0]);
